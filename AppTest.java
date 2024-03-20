7eb15ae83e76e4a4df1c1b35cf77083c44010981
erickgzg <163381220+erickgzg@users.noreply.github.com>
2024-03-21 00:28:00 +1100

    Update AppTest.java

--- AppTest.java
+++ AppTest.java
@@ -1,67 +1,30 @@
-package hello;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-import org.openqa.selenium.By;
-
-import org.openqa.selenium.WebDriver;
-
-import org.openqa.selenium.WebElement;
-
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import org.testng.Assert;
-
-import org.testng.annotations.AfterClass;
-
-import org.testng.annotations.BeforeClass;
-
-import org.testng.annotations.Test;
+package test;
 
 import java.util.concurrent.TimeUnit;
-
- 
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AppTest 
-
 { 
-
- public WebDriver driver; 
-
- public String baseUrl = "https://www.lambdatest.com/";  
-
+	public WebDriver driver; 
+	public String baseUrl = "https://www.lambdatest.com/";  
+	
+	@Test             
+	public void test1() {      
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--headless");
+		driver = new ChromeDriver(options);
  
-
- @Test             
-
- public void test1() {      
-
- 
-
- WebDriverManager.chromedriver().setup();
-
- ChromeOptions options = new ChromeOptions();
-
- options.addArguments("--no-sandbox");
-
- options.addArguments("--disable-dev-shm-usage");
-
- options.addArguments("--headless");
-
- driver = new ChromeDriver(options);
-
- 
-
- driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);  
-
- driver.manage().window().maximize();  
-
- driver.get(baseUrl);
-
- driver.close();
-
- }     
-
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);  
+		driver.manage().window().maximize();  
+		driver.get(baseUrl);
+		driver.close();
+	}     
 }
